08c94d82b255c63baab79f1680dfb6967493fc0b
Ambrella <154675435+AmbreIIa@users.noreply.github.com>
2024-11-24 02:12:56 +1400

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,30 @@
 from flask import *
+import sqlite3
 app = Flask(__name__)
+connection = sqlite3.connect("database.sqlite")
+cursor = sqlite3.Cursor(connection)
 
-@app.rout('/')
+def make_product_card(info):
+    products = cursor.execute("SELECT * FROM products").fetchall()
+    for product in products:
+        cards.append(make_product_card(product))
+    name = str(info[1])
+    description = str(info[2])
+    price =str(info[3]) + "UAH"
+
+    return f'<div class="services"> <div class="service-box"><h4>{name}</h4><p>{description}</p></div>'
+
+
+
+@app.route('/')
 def index():
     return render_template('index.html')
 
-@app.route('result', methods=['GET'])
+@app.route("/result")
 def result():
-    product = request.args.get('product')
-    return.render_template('result.html', text='product')
+
+    cards = make_product_card()
+
+    return render_template('product.html', products=cards)
 
 app.run(debag=True)
