ada3c6518f90313057749c5ef5b416467c2db441
Ambrella <154675435+AmbreIIa@users.noreply.github.com>
2024-11-24 02:13:21 +1400

    Update sql.py

--- sql.py
+++ sql.py
@@ -7,17 +7,18 @@ cursor = sqlite3.Cursor(connection)
 request = ("CREATE TABLE IF NOT EXISTS products"
            "(id INTEGER PRIMARY KEY AUTOINCREMENT,"
            "name VARCHAR(255),"
+           "description VARCHAR(255)"
            "price INTEGER)")
 
 
 cursor.execute(request)
 
 insert_request = ("INSERT INTO products"
-                  "(name,price) VALUES(?,?)")
+                  "(name,price) VALUES(?,?,?)")
 
-cursor.execute(insert_request, ("стрижка" , "270 грн"))
-cursor.execute(insert_request, ("фарбування " , "250 грн"))
-cursor.execute(insert_request, ("укладання" , "150 грн"))
+cursor.execute(insert_request, ("стрижка" , "Класичні та сучасні стрижки для будь-якого віку та стилю.", "270 грн"))
+cursor.execute(insert_request, ("фарбування " ,"Професійне фарбування з використанням якісної косметики.", "250 грн"))
+cursor.execute(insert_request, ("укладання" ,"Ідеальні укладки для будь-якої події – від повсякденних до весільних.", "150 грн"))
 
 text = cursor.execute("SELECT * FROM products")
 print(text.fetchall())
