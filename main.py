e17e73179da81e6610d0953d15dd2d20bcb0dfe7
LOGIKA <danilferman348@gmail.com>
2024-12-07 13:39:18 +0200

    1212

--- main.py
+++ main.py
@@ -18,7 +18,7 @@ def make_product_card(info):
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('main.html')
 
 @app.route("/result")
 def result():
