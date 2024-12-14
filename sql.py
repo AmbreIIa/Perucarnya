aecb6bcb23380bca59675d246f521219ea91a2e6
Ambrella <serbininzena152@gmail.com>
2024-12-14 11:54:58 +0200

    12

--- sql.py
+++ sql.py
@@ -20,5 +20,49 @@ cursor.execute(insert_request, ("стрижка" , "Класичні та суч
 cursor.execute(insert_request, ("фарбування " ,"Професійне фарбування з використанням якісної косметики.", "250 грн"))
 cursor.execute(insert_request, ("укладання" ,"Ідеальні укладки для будь-якої події – від повсякденних до весільних.", "150 грн"))
 
+request = ("CREATE TABLE IF NOT EXISTS strizhka"
+           "(id INTEGER PRIMARY KEY AUTOINCREMENT,"
+           "name VARCHAR(255),"
+           "description VARCHAR(255)")
+
+cursor.execute(request)
+
+insert_request = ("INSERT INTO strizhka"
+                  "(name,price) VALUES(?,?)")
+
+cursor.execute(insert_request, ("Марія 23 рооки" ,"Стаж роботи 2 роки"))
+cursor.execute(insert_request, ("Володимир 30 років" ,"Стаж роботи 5 років"))
+cursor.execute(insert_request, ("Анна 25 років" ,"Стаж роботи 4 роки"))
+
+
+request = ("CREATE TABLE IF NOT EXISTS ukladka"
+           "(id INTEGER PRIMARY KEY AUTOINCREMENT,"
+           "name VARCHAR(255),"
+           "description VARCHAR(255)")
+
+cursor.execute(request)
+
+insert_request = ("INSERT INTO ukladka"
+                  "(name,price) VALUES(?,?)")
+
+cursor.execute(insert_request, ("Олександр 28 рооки" ,"Стаж роботи 6 років"))
+cursor.execute(insert_request, ("Олександра 22 рока" ,"Стаж роботи 1 рік"))
+cursor.execute(insert_request, ("Василь 25 років" ,"Стаж роботи 4 роки"))
+
+
+request = ("CREATE TABLE IF NOT EXISTS farb"
+           "(id INTEGER PRIMARY KEY AUTOINCREMENT,"
+           "name VARCHAR(255),"
+           "description VARCHAR(255)")
+
+cursor.execute(request)
+
+insert_request = ("INSERT INTO farb"
+                  "(name,price) VALUES(?,?)")
+
+cursor.execute(insert_request, ("Марія 23 рооки" ,"Стаж роботи 2 роки"))
+cursor.execute(insert_request, ("Володимир 30 років" ,"Стаж роботи 5 років"))
+cursor.execute(insert_request, ("Анна 25 років" ,"Стаж роботи 4 роки"))
+
 text = cursor.execute("SELECT * FROM products")
 print(text.fetchall())
