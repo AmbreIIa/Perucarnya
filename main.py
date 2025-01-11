24c8d839246d3a1517e024242606bb3fa80306af
Ambrella <serbininzena152@gmail.com>
2025-01-11 20:58:36 +0200

    1231231

--- main.py
+++ main.py
@@ -25,28 +25,52 @@ def index():
 @app.route('/strizhkamasters')
 def strizhkamasters():
     conn = get_db_connection()
-    cursor = conn.cursor()
-    response = cursor.execute("SELECT * FROM strizhka")
-    masters = response.fetchall()
+    masters = conn.execute("SELECT * FROM strizhka").fetchall()
+    conn.close()
     return render_template("masters.html", masters=masters)
-
 
 @app.route('/farbmasters')
 def farbmasters():
     conn = get_db_connection()
-    cursor = conn.cursor()
-    response = cursor.execute("SELECT * FROM farb")
-    masters = response.fetchall()
+    masters = conn.execute("SELECT * FROM farb").fetchall()
+    conn.close()
     return render_template("masters.html", masters=masters)
-
 
 @app.route('/ukladmasters')
 def ukladmasters():
     conn = get_db_connection()
-    cursor = conn.cursor()
-    response = cursor.execute("SELECT * FROM ukladka")
-    masters = response.fetchall()
+    masters = conn.execute("SELECT * FROM ukladka").fetchall()
+    conn.close()
     return render_template("masters.html", masters=masters)
+
+@app.route('/appointment/<int:master_id>')
+def appointment(master_id):
+    conn = get_db_connection()
+    master = conn.execute(
+        "SELECT * FROM strizhka WHERE id = ? UNION "
+        "SELECT * FROM farb WHERE id = ? UNION "
+        "SELECT * FROM ukladka WHERE id = ?", (master_id, master_id, master_id)
+    ).fetchone()
+    conn.close()
+
+    if master:
+        return render_template("appointment.html", master=master)
+    else:
+        return "Мастер не найден", 404
+
+@app.route('/submit_appointment', methods=['POST'])
+def submit_appointment():
+    master_id = request.form.get('master_id')
+    date = request.form.get('date')
+    time = request.form.get('time')
+
+    conn = get_db_connection()
+    conn.execute("INSERT INTO appointments (master_id, date, time) VALUES (?, ?, ?)",
+                 (master_id, date, time))
+    conn.commit()
+    conn.close()
+
+    return redirect(url_for('index'))
 
 
 if __name__ == "__main__":
